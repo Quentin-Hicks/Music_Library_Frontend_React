ee8cfbae74b3383c4e66bd0eaaf3e144fd50413c
unknown <qwhick2@gmail.com>
2022-03-13 13:37:50 -0400

    Created Music Form

--- src/Components/Music.jsx
+++ src/Components/Music.jsx
@@ -3,12 +3,47 @@ import React, { useState } from 'react';
 
 const MusicForm = (props) => {
 
+    const [title, setTitle] = useState('')
+    const [artist, setArtist] = useState('')
+    const [album, setAlbum] = useState('')
+    const [releaseDate, setReleaseDate] = useState('')
+    const [genre, setGenre] = useState('')
 
-
+    function handleSubmit(event) {
+        event.preventDefault()
+        let newMusic = {
+            title: title,
+            artist: artist,
+            album: album,
+            releaseDate: releaseDate,
+            genre: genre
+        }
+        console.log(newMusic)
+    }
 
     return (
         <form>
-            
+            <div>
+                <label></label>
+                <input />
+            </div>
+            <div>
+                <label></label>
+                <input />
+            </div>
+            <div>
+                <label></label>
+                <input />
+            </div>
+            <div>
+                <label></label>
+                <input />
+            </div>
+            <div>
+                <label></label>
+                <input />
+            </div>
+            <button></button>
         </form>
     )
 }
