a17b6a5907cfc101c356b4b66e92cf73fdc25dd7
unknown <qwhick2@gmail.com>
2022-03-14 20:09:59 -0400

    Installed Axios + Created Async Function For Post Request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import MusicForm from "./Components/Music";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
 function App() {
+
+  const [songs, setSongs] = useState([])
+
+  function sendSong(){
+    let song = {
+      "id": 4,
+      "title": "Video",
+      "artist": "Indie.Arie",
+      "album": "",
+      "release_date": "2000-06-15",
+      "genre": "RNB"
+  }
+  createSong(song)
+  }
+
+  async function createSong(newSong){
+    let response = await axios.post('http://127.0.0.1:8000/api/music/', newSong)
+    console.log(response.data)
+    setSongs(response.data)
+  }
+
   return (
     <div>
       <h3>My Music</h3>
-      <div>
-        <MusicForm />
+      <div className='App'>
+        <button onClick={() => sendSong()}>Submit</button>
       </div>
     </div>
   );
